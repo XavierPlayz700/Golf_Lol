e839be43d0b7f0937ced59603e4c5f5b8ab08b57
Kokichi <75687135+XavierPlayz700@users.noreply.github.com>
2021-07-07 10:28:08 +0530

    Update GolfDriver.js

--- GolfDriver.js
+++ GolfDriver.js
@@ -7,7 +7,7 @@ class Driver {
       isStatic : true
     }
 
-    this.body = Bodies.rectangle(x, y, 200, 20, options);
+    this.body = Bodies.rectangle(x, y, 250, 20, options);
     this.width = 200;
     this.height = 20;
     World.add(world, this.body);
